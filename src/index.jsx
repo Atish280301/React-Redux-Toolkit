04b824374c4d98a54cd23facd56c0d63e9b8fbf5
atishoff012803 <kumarsahuatish280301@gmail.com>
2024-03-03 12:35:45 +0530

    Commit - 04
    Connect Redux Tool Kit With React Using <Provider></Provider>

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import Store from './Store/Slices/Store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={Store}>
+        <App />
+    </Provider>
+);
